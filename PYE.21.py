04cd83084aa84b13d4c6a5b6d2167c3e7de53530
GURU K <gurunathangurunathan006@gmail.com>
2025-09-29 10:19:05 +0530

    Add files via upload

---
+++ PYE.21.py
@@ -0,0 +1,8 @@
+print("Square Program")
+print("------------------")
+n=int(input ("Enter the number:"))
+for i in range (n+5):
+    for j in range (n):
+        print("*",end="")
+    print()  
+ 
---
+++ PYE.22.py
@@ -0,0 +1,11 @@
+print("Triangle Program")
+print("------------------")
+n=int(input ("Enter the number:"))
+for i in range (n+1):
+    for j in range (i):
+        print("*",end="")
+    print()  
+for i in range (n,0-1):
+    for j in range (i):
+        print("*",end="")
+    print()  
